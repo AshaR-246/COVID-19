ec786b10a1cb64dfde0427dca96ef250f1c5c7a9
AshaR-246 <62698273+AshaR-246@users.noreply.github.com>
2020-04-16 11:44:45 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,106 @@
+onEvent("home_button_one", "click", function( ) {
+  setScreen("stay_safe");
+});
+onEvent("stay_safe_button", "click", function( ) {
+  setScreen("home");
+});
+onEvent("home_button_two", "click", function( ) {
+  setScreen("me_safe");
+});
+onEvent("me_safe_button", "click", function( ) {
+  setScreen("home");
+});
+onEvent("home_button_three", "click", function( ) {
+  setScreen("symptoms");
+});
+onEvent("symptoms_butto", "click", function( ) {
+  setScreen("home");
+});
+onEvent("button1", "click", function( ) {
+setScreen("screen1");
+    
+});
+onEvent("button5", "click", function( ) {
+  setScreen("HowItSpreads");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button11", "click", function( ) {
+  setScreen("home");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button9", "click", function( ) {
+  setScreen("screen7");
+});
+
+onEvent("button10", "click", function( ) {
+  setScreen("home");
+});
+onEvent("button2", "click", function( ) {
+setScreen("screen8");
+    
+});
+
+onEvent("button16", "click", function( ) {
+open("https://www.who.int");
+
+});
+onEvent("button12", "click", function( ) {
+open("https://www.accuweather.com/en/in/national/covid-19");
+    
+});
+onEvent("button13", "click", function( ) {
+open("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/global-research-on-novel-coronavirus-2019-ncov");
+    
+});
+onEvent("button14", "click", function( ) {
+ open ("https://news.google.com/covid19/map?hl=en-IN&gl=IN&ceid=IN:en");
+   
+});
+onEvent("button17", "click", function( ) {
+open("https://www.mygov.in/aarogya-setu-app/");
+    
+});
+onEvent("button4", "click", function( ) {
+setScreen("home");
+    
+});
+onEvent("button18", "click", function( ) {
+setScreen("screen3");
+    
+});
+
+
+onEvent("button19", "click", function( ) {
+open("https://www.who.int/timorleste/#");
+    
+});
+onEvent("button20", "click", function( ) {
+setScreen("home");
+});
+onEvent("button3", "click", function( ) {
+setScreen("HowItSpreads");
+});
+onEvent("button21", "click", function( ) {
+setScreen("screen4");
+    
+});
+onEvent("button22", "click", function( ) {
+setScreen("screen5");
+});
+onEvent("button23", "click", function( ) {
+setScreen("home");
+});
+onEvent("button24", "click", function( ) {
+open("https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf");
+
+    
+});
+
+
